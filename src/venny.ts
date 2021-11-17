5791e14251a66ca6ae7108078020fbdb4272736e
Preet <833927+pshihn@users.noreply.github.com>
2021-11-17 15:28:00 -0800

    .

--- src/venny.ts
+++ src/venny.ts
@@ -2,8 +2,6 @@ import './venn-diagram.js';
 import './venn-set';
 import './venn-intersection';
 
-// const sets: AreaDetails[] = [
-//   { sets: ['A'], size: 12 },
-//   { sets: ['B'], size: 12 },
-//   { sets: ['A', 'B'], size: 2 },
-// ];
+export * from './venn-diagram.js';
+export * from './venn-set';
+export * from './venn-intersection';
